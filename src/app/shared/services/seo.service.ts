0f007155672d235e590fc6b06f3bcd96d4bcfe1b
pavelpleshko <pavelpleshko92@mail.ru>
2018-09-21 22:19:07 +0300

    seo.fix

--- src/app/shared/services/seo.service.ts
+++ src/app/shared/services/seo.service.ts
@@ -33,10 +33,12 @@ export class SeoService {
 	}
 
 createLinkForCanonicalURL(){
-	let link: HTMLLinkElement = this.doc.createElement('link');
-    link.setAttribute('rel', 'canonical');
-    this.doc.head.appendChild(link);
-    link.setAttribute('href', this.doc.URL);       
+	if(typeof this.doc != 'undefined' && this.doc.createElement){
+		let link: HTMLLinkElement = this.doc.createElement('link');
+	    link.setAttribute('rel', 'canonical');
+	    this.doc.head.appendChild(link);
+	    link.setAttribute('href', this.doc.URL);  
+	}     
 }
 
 }
