0f1455bd2a65db2ccc3f0eef5547dfe0c3c24a99
pavelpleshko <pavelpleshko92@mail.ru>
2018-09-21 23:48:40 +0300

    seo.fix

--- src/app/shared/services/seo.service.ts
+++ src/app/shared/services/seo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable,Inject } from '@angular/core';
 import {NavigationEnd,Router,RouterEvent,ActivatedRoute} from '@angular/router';
 import { Meta, Title } from '@angular/platform-browser';
-import {filter,map,mergeMap} from 'rxjs/operators';
+import {filter,map,mergeMap,tap} from 'rxjs/operators';
 import { DOCUMENT } from '@angular/common';
 
 import {defaultMetaItem} from '../../seo/metaData';
@@ -19,6 +19,7 @@ canonicalLinks:any={};
 		private titleService:Title
 		){
 		this.router.events.pipe(
+			tap(()=>this.createLinkForCanonicalURL()),
 			filter((event:RouterEvent)=>event instanceof NavigationEnd),
 			map(event=>{
 				let route = this.route
@@ -29,7 +30,6 @@ canonicalLinks:any={};
 			mergeMap(route=>route.data)
 			)
 		.subscribe((event)=>{
-				this.createLinkForCanonicalURL();
 				this.titleService.setTitle(event['title'] || defaultMetaItem.title);
 				this.metaService.updateTag({name:'description',content:event['desc'] || defaultMetaItem.desc});
 		})
@@ -38,21 +38,23 @@ canonicalLinks:any={};
 createLinkForCanonicalURL(){
 	try{
 		let url:string = this.doc.URL;
-		if(this.canonicalLinks[url]){
+		if(!!this.canonicalLinks[url]){
 			return;
 		}else{
-			this.canonicalLinks[url]=true;
+			for(let key in this.canonicalLinks){
+				let el = this.canonicalLinks[key];
+				this.doc.head.removeChild(el);
+				this.canonicalLinks[key]=undefined;
+			}
+			
 			let link: HTMLLinkElement = this.doc.createElement('link');
 		    link.setAttribute('rel', 'canonical');
 		    this.doc.head.appendChild(link);
 		    link.setAttribute('href', url);  
+		    this.canonicalLinks[url]=link;
 		}
-		
-	}catch(err){
-
-	}
-
-	}     
+	}catch(err){}
+}     
 }
 
 
